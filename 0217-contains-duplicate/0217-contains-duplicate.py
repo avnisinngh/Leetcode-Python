b9116585cce5ede1347ba2615328caf9995d2dd8
Avni Singh <150375257+avnisinngh@users.noreply.github.com>
2025-09-27 10:44:08 +0530

    Time: 23 ms (21.05%), Space: 34.9 MB (9.91%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,9 +1,10 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        list_set = set()
+        list_map = {}
         for num in nums:
-            if num in list_set:
+            if num in list_map and list_map[num] >= 1:
                 return True
-            list_set.add(num)
+            list_map[num] = list_map.get(num, 0)+1
+        return False
 
         return False
