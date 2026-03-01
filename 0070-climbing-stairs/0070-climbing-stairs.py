ed98405c6def918ed489fbbff6fcd12cd570d883
Avni Singh <150375257+avnisinngh@users.noreply.github.com>
2026-03-01 11:47:13 +0530

    Time: 0 ms (100%), Space: 19.3 MB (78.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,11 @@
 class Solution:
-    # memoization 
+    # tabulation
     def climbStairs(self, n: int,dp=None) -> int:
-        if dp is None:
-            dp = {}
-        if n in dp:
-            return dp[n]
-        if n==0 or n==1:
-            return 1
-        dp[n] = self.climbStairs(n-1, dp)+self.climbStairs(n-2,dp)
+        dp = {}
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
         return dp[n]
 
         
