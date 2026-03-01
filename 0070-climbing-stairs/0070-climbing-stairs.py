061f48c12898f7cc808e42c01bcb8a86c5d9d600
Avni Singh <150375257+avnisinngh@users.noreply.github.com>
2026-03-01 11:50:21 +0530

    Time: 0 ms (100%), Space: 19.1 MB (87.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,12 @@
 class Solution:
-    # tabulation
+    # space optimization
     def climbStairs(self, n: int,dp=None) -> int:
-        dp = {}
-        dp[0] = 1
-        dp[1] = 1
-        for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
+        prev1 = 1
+        prev2 = 1
+        for _ in range(2, n+1):
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
+        return prev1
 
         
